1275116df75b7295f5ded6bf3374f82c10b3ae45
YunSeobLee <fhzotxldj@gmail.com>
2019-07-05 14:02:44 -0400

    add source files for 'Group Anagrams'

--- Top Interview Questions/Medium Collection/Array and String/Group Anagrams.py
+++ Top Interview Questions/Medium Collection/Array and String/Group Anagrams.py
@@ -1,9 +1,18 @@
 """
-URL :
+URL : https://leetcode.com/explore/interview/card/top-interview-questions-medium/103/array-and-strings/778/
 """
+import collections
 
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        m = collections.defaultdict(list)
+        for s in strs:
+            m[''.join(sorted(s))].append(s)
+        return list(m.values())
 
-class Solution:
-
-
-if __name__ == "__main__":
+s = Solution()
+print(s.groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
