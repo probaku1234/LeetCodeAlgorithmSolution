b58c1d886ead6d9a6774fb61a6696461d007a261
probaku1234 <fhzotxldj@gmail.com>
2019-07-16 16:11:22 -0400

    add source files for 'Sum of Two Integers'

---
+++ Top Interview Questions/Medium Collection/Others/Sum of Two Integers.py
@@ -0,0 +1,19 @@
+"""
+URL : https://leetcode.com/explore/interview/card/top-interview-questions-medium/114/others/822/
+"""
+
+class Solution(object):
+    def getSum(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        MAX_INT = 0x7FFFFFFF
+        MIN_INT = 0x80000000
+        MASK = 0x100000000
+
+        while b:
+            a, b = (a ^ b) % MASK, ((a & b) << 1) % MASK
+        return a if a <= MAX_INT else ~((a % MIN_INT) ^ MAX_INT)
+
