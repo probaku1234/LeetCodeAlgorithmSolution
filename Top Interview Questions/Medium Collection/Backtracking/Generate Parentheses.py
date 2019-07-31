932f5114d5162d825e9587a8d93d006f1981d056
probaku1234 <fhzotxldj@gmail.com>
2019-07-31 14:09:16 -0400

    add source files for 'Generate Parentheses'

---
+++ Top Interview Questions/Medium Collection/Backtracking/Generate Parentheses.py
@@ -0,0 +1,68 @@
+"""
+URL : https://leetcode.com/explore/interview/card/top-interview-questions-medium/109/backtracking/794/
+"""
+
+
+class Solution(object):
+    def is_valid(self, s):
+        if len(s) == 0:
+            return True
+
+        stack = []
+
+        for p in s:
+            if p == '(':
+                stack.append(p)
+            else:
+                if len(stack) == 0:
+                    return False
+                if p == ')':
+                    if stack[-1] == '(':
+                        stack.pop()
+                    else:
+                        return False
+
+        return len(stack) == 0
+
+    def is_a_solution(self, answer, step, n):
+        if step == n * 2:
+            return self.is_valid(''.join(answer))
+        else:
+            return False
+
+    def process_solution(self, answer, step, output):
+        output.append(''.join(answer))
+
+    def construct_candidates(self, step, input, candidates):
+        candidates[0] = '('
+        candidates[1] = ')'
+
+    def backtrack(self, answer, step, input, output):
+        candidates = ['a'] * 2
+
+        if self.is_a_solution(answer, step, input):
+            self.process_solution(answer, step, output)
+        else:
+            step += 1
+            if step > input * 2:
+                return
+            self.construct_candidates(step, input, candidates)
+            for i in range(len(candidates)):
+                answer[step-1] = candidates[i]
+                self.backtrack(answer, step, input, output)
+
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        answer = ['a'] * (n * 2)
+        output = []
+
+        self.backtrack(answer, 0, n, output)
+
+        return output
+
+
+s = Solution()
+print s.generateParenthesis(3)
---
+++ Top Interview Questions/Medium Collection/Backtracking/Permutations.py
@@ -0,0 +1,53 @@
+"""
+URL : https://leetcode.com/explore/interview/card/top-interview-questions-medium/109/backtracking/795/
+"""
+import copy
+
+class Solution(object):
+    def is_solution(self, step, length):
+        return step == length
+
+    def process_solution(self, answer, output):
+        temp = copy.deepcopy(answer)
+        output.append(temp)
+
+    def construct_candidates(self, answer, step, input, candidates):
+        in_perm = {num: False for num in input}
+
+        for i in range(step):
+            if answer[i] != 'a':
+                in_perm[answer[i]] = True
+
+        for key, value in in_perm.items():
+            if not value:
+                candidates.append(key)
+
+    def backtracking(self, answer, step, input, output):
+        candidates = []
+
+        if self.is_solution(step, len(input)):
+            self.process_solution(answer, output)
+        else:
+            step += 1
+            self.construct_candidates(answer, step, input, candidates)
+            for i in range(len(candidates)):
+                answer[step-1] = candidates[i]
+                self.backtracking(answer, step, input, output)
+
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        if len(nums) == 0:
+            return []
+
+        answer = ['a'] * len(nums)
+        output = []
+
+        self.backtracking(answer, 0, nums, output)
+
+        return output
+
+s = Solution()
+print s.permute([1,2,3])
