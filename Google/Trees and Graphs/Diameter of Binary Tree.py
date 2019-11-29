06e6b40796efb0205009ec575dbb1661fa79ed10
Yun Seob Lee <fhzotxldj@gmail.com>
2019-11-29 17:27:31 -0500

    add source files

---
+++ Google/Trees and Graphs/Diameter of Binary Tree.py
@@ -0,0 +1,29 @@
+"""
+URL : https://leetcode.com/explore/interview/card/google/61/trees-and-graphs/3074/
+"""
+
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def diameterOfBinaryTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        self.ans = 1
+
+        def depth(node):
+            if not node: return 0
+            L = depth(node.left)
+            R = depth(node.right)
+            self.ans = max(self.ans, L+R+1)
+            return max(L, R) + 1
+
+        depth(root)
+        return self.ans - 1
