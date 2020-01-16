4179597640d61e793184575285d7732dbad96925
Yun Seob Lee <fhzotxldj@gmail.com>
2020-01-16 01:53:59 -0500

    modify Integer to Roman

--- Amazon/Arrays and Strings/Integer to Roman.py
+++ Amazon/Arrays and Strings/Integer to Roman.py
@@ -10,46 +10,39 @@ class Solution(object):
         :rtype: str
         """
         output = ''
-        int_to_roman_dict_one = {
+        int_to_roman_dict = {
             1: 'I',
             4: 'IV',
             5: 'V',
-            9: 'IX'
-        }
-        int_to_roman_dict_two = {
+            9: 'IX',
             10: 'X',
             40: 'XL',
             50: 'L',
-            90: 'XC'
-        }
-        int_to_roman_dict_three = {
+            90: 'XC',
             100: 'C',
             400: 'CD',
             500: 'D',
-            900: 'CM'
-        }
-        int_to_roman_dict_four = {
+            900: 'CM',
             1000: 'M'
         }
-        dict_list = [int_to_roman_dict_one, int_to_roman_dict_two, int_to_roman_dict_three, int_to_roman_dict_four]
+
         num_string = str(num)
         n = len(num_string)
 
         for i in range(n):
             if num_string[i] == '0':
                 continue
-            number = int(num_string[i]) * (10 ** (n - i - 1))
-            num_dict = dict_list[n - i - 1]
+            number = int(num_string[i])
 
-            if num_dict.get(number) is not None:
-                output += num_dict[number]
-            elif number < 4 * (10 ** (n - i - 1)):
+            if int_to_roman_dict.get(number * (10 ** (n - i - 1))) is not None:
+                output += int_to_roman_dict[number * (10 ** (n - i - 1))]
+            elif number < 4:
                 for j in range(int(num_string[i])):
-                    output += num_dict[10 ** (n - i - 1)]
-            elif number > 5 * (10 ** (n - i - 1)):
-                output += num_dict[5 * (10 ** (n - i - 1))]
+                    output += int_to_roman_dict[10 ** (n - i - 1)]
+            elif number > 5:
+                output += int_to_roman_dict[5 * (10 ** (n - i - 1))]
                 for j in range(int(num_string[i]) - 5):
-                    output += num_dict[10 ** (n - i - 1)]
+                    output += int_to_roman_dict[10 ** (n - i - 1)]
 
         return output
 
