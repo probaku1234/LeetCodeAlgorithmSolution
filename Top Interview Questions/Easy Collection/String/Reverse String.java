52ace69c3cf12bfdb42a449e51715e5629a936b4
Yun Seob Lee <fhzotxldj@gmail.com>
2022-08-12 18:33:43 -0400

    add Reverse String java version

---
+++ Top Interview Questions/Easy Collection/String/Reverse String.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void reverseString(char[] s) {
+        if (s.length == 1){
+            return;
+        }
+
+        int i = 0;
+        int j = s.length - 1;
+
+        while ( i < j) {
+            char temp = s[j];
+            s[j] = s[i];
+            s[i] = temp;
+
+            i++;
+            j--;
+        }
+    }
+}
